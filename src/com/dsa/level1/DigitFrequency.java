c06eece2d307103b4019e321e8da3c8d8e0db857
rahul2007prasad <107prasad107@gmail.com>
2024-05-23 12:30:09 +0530

    digit frequency

--- src/com/dsa/level1/DigitFrequency.java
+++ src/com/dsa/level1/DigitFrequency.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class DigitFrequency {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int d = sc.nextInt();
+		int res=0;
 		
+		while(n > 0) {
+			int dig  = n % 10;
+			n= n / 10;
+			
+			if(dig  == d) {
+				res++;
+			}
+		}
+		
+			System.out.println(res);
 	}
 }
