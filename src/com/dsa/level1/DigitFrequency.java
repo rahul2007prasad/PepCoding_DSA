a91e124239d2588a041084d20e87f551747c9caa
rahul2007prasad <107prasad107@gmail.com>
2024-05-23 12:25:28 +0530

    npr fact fun

---
+++ src/com/dsa/level1/DigitFrequency.java
@@ -0,0 +1,10 @@
+package com.dsa.level1;
+
+import java.util.Scanner;
+
+public class DigitFrequency {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
---
+++ src/com/dsa/level1/NPRFact.java
@@ -0,0 +1,36 @@
+package com.dsa.level1;
+
+import java.util.Scanner;
+
+public class NPRFact {
+	
+	public static int f(int x) {
+		int xsquare = x * x;
+		return xsquare;
+	}
+	public static int fact(int x) {
+		int rv;
+		for(int i =1 i <=x ; i++) {
+			rv = rv * i;
+		}
+		return rv;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int r = sc.nextInt();
+		int nfact = 1;
+		
+		for(int i =1  ;  i <= n ; i++) {
+			nfact = nfact * i;
+		}
+		
+		int nmrfact =1;
+		for(int i = 1 ; i  <= n-r ; i ++) {
+			nmrfact = nmrfact * i;
+		}
+		
+		int npr = nfact / nmrfact;
+		System.out.println(n + "P" + r + " = " + npr);
+	}
+}
