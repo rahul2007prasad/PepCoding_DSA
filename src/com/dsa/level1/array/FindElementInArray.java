08d06f47d6bc12ca7bb7e68d2de455efa637c54a
rahul2007prasad <107prasad107@gmail.com>
2024-05-23 15:39:01 +0530

    find elmnt in array

---
+++ src/com/dsa/level1/array/FindElementInArray.java
@@ -0,0 +1,25 @@
+package com.dsa.level1.array;
+
+import java.util.Scanner;
+
+public class FindElementInArray {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i < arr.length ; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int data = sc.nextInt();
+		int idx = -1;
+		for(int i=0 ; i< arr.length ; i++) {
+			if(arr[i] == data) {
+				idx = i;
+				break;
+			}
+		}
+		System.out.println("Index : "+ idx);
+		
+	}
+}
