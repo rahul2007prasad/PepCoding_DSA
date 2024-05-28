88b977f280f1b7dc83df144f9714e8c72f5a45c8
rahul2007prasad <107prasad107@gmail.com>
2024-05-28 22:33:29 +0530

    k level down

--- src/com/dsa/level1/binarytree/BinaryTree.java
+++ src/com/dsa/level1/binarytree/BinaryTree.java
@@ -229,6 +229,25 @@ public class BinaryTree {
 	 * }
 	 */
 	
+	//print k level down
 	
+	public static void printKLevelsDown(Node node , int k) {
+		if(node == null || k < 0) {
+			return ;
+		}
+		
+		if(k == 0) {
+			System.out.println(node.data);
+		}
+		printKLevelsDown(node.left, k - 1);
+		printKLevelsDown(node.right, k - 1);
+	}
+	
+	//print k level far
+	
+	public static void printKLevelFar(Node node , int k) {
+		
+		
+	}
 	
 }
