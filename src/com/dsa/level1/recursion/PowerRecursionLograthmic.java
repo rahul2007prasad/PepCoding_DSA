d7180cce3aa92636e46a65fba76cc6481bf0aed8
rahul2007prasad <107prasad107@gmail.com>
2024-06-02 11:31:01 +0530

    power logritmic

--- src/com/dsa/level1/recursion/PowerRecursionLograthmic.java
+++ src/com/dsa/level1/recursion/PowerRecursionLograthmic.java
@@ -2,16 +2,15 @@ package com.dsa.level1.recursion;
 
 import java.util.Scanner;
 
-public class PowerRecursion {
+public class PowerRecursionLograthmic {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		int n = sc.nextInt();
 		
+		//x to pow n = (x to pow n/2 )* (x to pow n/2)
 		
 		int pow = pow(x , n);
-		
-		// x to power n = x * x to powr n-1
 		System.out.println(x + " pow  " + n + " : "+ pow);
 	}
 	
@@ -19,8 +18,11 @@ public class PowerRecursion {
 		if(n == 0) {
 			return 1;
 		}
-		int xnm1 = pow(x , n - 1);
-		int xn = x * xnm1;
+		int xpnb2 = pow(x , n / 2);
+		int xn = xpnb2 * xpnb2;
+		if(n % 2 == 1) {
+			xn = xn * x;
+		}
 		return xn;
 		
 	}
