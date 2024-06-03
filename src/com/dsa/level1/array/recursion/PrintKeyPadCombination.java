007d675b49f5a00e9d65af368cfc09212691f028
rahul2007prasad <107prasad107@gmail.com>
2024-06-03 22:12:52 +0530

    print subsequnce

--- src/com/dsa/level1/array/recursion/PrintKeyPadCombination.java
+++ src/com/dsa/level1/array/recursion/PrintKeyPadCombination.java
@@ -9,8 +9,23 @@ public class PrintKeyPadCombination {
 		printKPC(str ,"");
 	}
 	
+	static String[] codes = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+	
+	
 	public static void printKPC(String ques , String ans) {
+		if(ques.length() == 0) {
+			System.out.print(ans + " ");
+			return;
+		}
 		
+		char ch = ques.charAt(0);
+		String roq = ques.substring(1);
+		
+		String codeforch = codes[ch - '0'];
+		for(int i =0 ; i < codeforch.length() ; i++) {
+			char cho = codeforch.charAt(i);
+			printKPC(roq, ans + cho);
+		}
 		
 	}
 }
