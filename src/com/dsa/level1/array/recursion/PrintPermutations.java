3964ebb57d580754e03e5504c6eb4175f81f20d6
rahul2007prasad <107prasad107@gmail.com>
2024-06-04 21:01:21 +0530

    print permutations

--- src/com/dsa/level1/array/recursion/PrintPermutations.java
+++ src/com/dsa/level1/array/recursion/PrintPermutations.java
@@ -1,5 +1,28 @@
 package com.dsa.level1.array.recursion;
 
-public class PrintPermutations {
+import java.util.Scanner;
 
+public class PrintPermutations {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		printPermutations(str,"");
+	}
+	
+	public static void printPermutations(String ques , String asf) {
+		
+		if(ques.length() == 0) {
+			System.out.print(asf + "  ");
+			return;
+		}
+		
+		for(int i =0 ; i < ques.length() ; i++) {
+			char ch = ques.charAt(i);
+			String qlpart = ques.substring(0 , i);
+			String qrpart = ques.substring(i + 1);
+			String roq = qlpart + qrpart;
+			printPermutations(roq, asf +  ch);
+		}
+		
+	}
 }
