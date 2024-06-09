ba3301780407e23f7c5f5b29ca1ced69a0590a58
rahul2007prasad <107prasad107@gmail.com>
2024-06-09 18:13:30 +0530

    path with max gold

---
+++ src/com/dsa/level1/bst/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package com.dsa.level1.bst;
+
+public class BinarySearchTree {
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int data , Node left , Node right){
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
