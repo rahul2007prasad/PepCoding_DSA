88079f77c9e130926489ab202ed70f4c6aee72f0
rahul2007prasad <107prasad107@gmail.com>
2024-06-09 18:27:23 +0530

    bst construct

--- src/com/dsa/level1/bst/BinarySearchTree.java
+++ src/com/dsa/level1/bst/BinarySearchTree.java
@@ -12,4 +12,38 @@ public class BinarySearchTree {
 			this.right = right;
 		}
 	}
+	
+	public static void display(Node node) {
+		if(node == null) {
+			return;
+		}
+		String str ="";
+		
+		str += node.left == null ? "." : node.left.data + "";
+		str += " <- " + node.data + " -> ";
+		str += node.right == null ? "." : node.right.data +"";
+		System.out.println(str);
+		display(node.left);
+		display(node.right);
+	}
+	
+	public static Node constructor(int[] arr , int low , int high ) {
+		if(low > high) {
+			return null;
+		}
+		
+		int mid  = low + (high - low) /2;
+		int data = arr[mid];
+		Node lc = constructor(arr, low, mid -1);
+		Node rc = constructor(arr, mid + 1, high);
+		Node node = new Node(data , lc , rc);
+		
+		return node;
+	}
+	
+	public static void main(String[] args) {
+		int [] arr = {12 , 25 , 37 , 50 , 62 , 75 , 87};
+		Node root = constructor(arr , 0 , arr.length - 1);
+		display(root);
+	}
 }
